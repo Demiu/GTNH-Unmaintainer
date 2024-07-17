0906857d07e382921db7132e85b0cbef1f8daade
Demiu <miazgapow@gmail.com>
2024-07-17 21:12:27 +0200

    Better dump, dump CPUs

--- test.lua
+++ test.lua
@@ -1,20 +1,57 @@
+local serialization = require('serialization')
 local component = require('component')
+local ME = component.me_interface
+
+function is_array(tbl) return type(tbl) == 'table' and tbl[1] ~= nil end
+
+function dump(o)
+    if type(o) == 'table' then
+        if is_array(o) then
+            print '['
+            for _,v in ipairs(o) do
+                dump(v)
+                print(',')
+            end
+            print ']'
+        else
+            print '{'
+            for k, v in pairs do
+                print(string.format('"%s":', k))
+                dump(v)
+                print(',')
+            end
+            print '}'
+        end
+    elseif type(o) == string
+        print('"%s"', o)
+    else
+        print(tostring(o))
+    end
+end
+
+--print(dump(component))
+for address,component_type in component.list('level_maintainer') do
+    print(address)
+    print(component_type)
+end
+
 
 function dump(o)
     if type(o) == 'table' then
        local s = '{ '
        for k,v in pairs(o) do
           if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
+          s = s .. k .. ': ' .. dump(v) .. ','
        end
        return s .. '} '
     else
-       return tostring(o)
+       return '"'..tostring(o)..'"'
     end
 end
 
-print(dump(component))
-for address,component_type in component.list('level_maintainer') do
+--print(dump(component))
+for address,component_type in component.list() do
     print(address)
     print(component_type)
 end
+dump(ME.getCpus())
